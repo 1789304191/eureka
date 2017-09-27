7dfc95f3444b116818ded0320e8b95ad41b75f7a
liguoxiong <liguoxiong-it@bestpay.com.cn>
2017-09-27 14:59:13 +0800

    分布式配置中心

--- config-client/src/main/java/com/lgx/ConfigClientApplication.java
+++ config-client/src/main/java/com/lgx/ConfigClientApplication.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@EnableEurekaClient
+//@EnableEurekaClient
 @RestController
 @RefreshScope
 public class ConfigClientApplication {
